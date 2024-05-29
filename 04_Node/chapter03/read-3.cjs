8459b00d364ee1d231e4d6321631d98340a6baec
Lee Gyu Bin <lcb1638@naver.com>
2024-05-29 16:34:50 +0900

    Vue

--- 04_Node/chapter03/read-3.cjs
+++
@@ -1,8 +0,0 @@
-fs = require('fs');
-
-const data = fs.readFileSync('./example.txt', 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(data);
-});
--- 04_Node/chapter03/write-1.cjs
+++
@@ -1,4 +0,0 @@
-fs = require('fs');
-
-const data = fs.readFileSync('./example.txt', 'utf-8'); // 인코딩 지정
-fs.writeFileSync('./text-1.txt', data);
